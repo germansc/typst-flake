fe07c577c1f60ba844f4a0703a31727f9c74f39f
germansc <germanscillone@gmail.com>
2025-08-25 12:03:24 -0300

    Initial flake definition for typst dev-environment.

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Typst Dev Environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      # Development Shell
+      devShells.${system}.default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.typst    # Core typst package
+          pkgs.tinymist # Typst LSP
+          pkgs.typstyle # Typst Formatter
+        ];
+        shellHook = ''
+          echo "Typst dev environment ready"
+          typst --version
+          tinymist -V
+          echo "typstyle $(typstyle --version | grep Version | tr -s ' ')"
+        '';
+      };
+    };
+}
